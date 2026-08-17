45c25ca47d3b0ed32856a258b167c1e6fffd1ccd
VineetKale <vineetkale31@gmail.com>
2026-08-17 22:05:07 +0530

    can now solve all arithmetic problems

--- html/arithmetic/solve.js
+++ html/arithmetic/solve.js
@@ -1,14 +1,32 @@
 const express=require("express")
 const app=express();
 
-app.get("/multiply",function(req,res){
-    const a=req.query.a;
-    const b=req.query.b;
+app.get("/multiply/:a/:b",function(req,res){
+    const a=req.params.a;
+    const b=req.params.b;
     res.json({
         answer:a*b
     })
 })
-app.get("/add",function(req,res){
-
+app.get("/add/:a/:b",function(req,res){
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+    res.json({
+        answer:a+b
+    })
+})
+app.get("/subtract/:a/:b",function(req,res){
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+    res.json({
+        answer:a-b
+    })
+})
+app.get("/divide/:a/:b",function(req,res){
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+    res.json({
+        answer:a/b
+    })
 })
 app.listen(3000);
