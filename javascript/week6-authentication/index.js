e585e725255f40e4569b61431e59fc2f96667a90
VineetKale <vineetkale31@gmail.com>
2026-08-20 22:02:34 +0530

    signup and signin authentication using express

---
+++ javascript/week6-authentication/index.js
@@ -0,0 +1,54 @@
+const express=require("express");
+const app=express();
+
+app.use(express.json())
+const users=[];
+
+function generateToken(){
+    let options="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let token="";
+    for(let i=0;i<32;i++){
+        token+=options[Math.floor(Math.random()*options.length)];
+    }
+    return token;
+}
+
+app.post("/signup",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    
+    users.push({
+        username:username,
+        password:password
+    })
+    res.json({
+        message:"You are signed in!"
+    })
+    console.log(users);
+})
+
+app.post("/signin",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    const founduser=users.find(function(u){
+        if(u.username==username&& u.password==password){
+            return true;
+        }
+        else return false;
+    })
+    if(founduser){
+        const token=generateToken();
+        founduser.token=token;
+        res.send({
+            token
+        })
+        console.log(founduser);
+    }
+    else{
+        res.status(403).send({
+            message:"Invalid username or password"
+        })
+    }
+    console.log(users);
+})
+app.listen(3000);
